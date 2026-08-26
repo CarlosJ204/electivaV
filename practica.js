b02d30774baa92984fb2fb18073f719412e24595
Carlos J <carlosgjimenez2004@gmail.com>
2026-08-26 18:43:11 -0500

    ejercicios en clase

---
+++ practica.js
@@ -0,0 +1,13 @@
+let  operacion1 = (3-2)*(10/2)
+let resultado1 = 5
+let operacion2 = 3-2*10/2
+let resultado2 = -7
+let operacion3 = (3-2)*10/2
+let resultado3 =  5
+
+console.log("El resultado de la operacion 1 es: " + operacion1 + " y el resultado que creo que  es: " + resultado1)
+console.log("El resultado de la operacion 2 es: " + operacion2 + " y el resultado que creo que  es: " + resultado2)
+console.log("El resultado de la operacion 3 es: " + operacion3 + " y el resultado que creo que  es: " + resultado3)
+
+console.log(resultado1--)
+console.log(resultado2++)
