8c630bf9af91fd6319ad9e384eee940a85b693c5
Juan Narvaez <jsnarvaez@unimayor.edu.co>
2026-09-17 20:46:50 -0500

    Login FINOVA

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -73,7 +73,39 @@ res.status(201).json({
 });
 
 router.post("/login", async (req, res) => {
-  
-}); 
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      message: "Logged in successfully",
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      }
+    });
+  } catch (error) {
+    console.log("Error al iniciar sesión:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 export default router;
